94f29d47728278b7fba6e7dfbfe286d913924cb8
maans-dev <hprademeyer123@gmail.com>
2022-07-17 18:56:28 +0200

    updates

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
 import './contact.css'
+import {ImMail4} from 'react-icons/im'
+import {BsDiscord} from 'react-icons/bs'
+import {FaLinkedin} from 'react-icons/fa'
+
 const contact = () => {
   return (
-    <section id='contacts'></section>
+    <section id='contacts'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact options'>
+          <article className='contact option'>
+              <ImMail4 className='contact__option-icon'/>
+              <h4>Email</h4>
+              <h5>hprademeyer123@gmail.com</h5>
+              <a href="mailto:hprademeyer123@gmail.com">Send me mail.</a>
+          </article>
+
+          <article>
+              <BsDiscord className='contact__option-icon'/>
+              <h4>Discord</h4>
+              <h5>https://discord.gg/MV8gQXvP</h5>
+              <a href="https://discord.gg/MV8gQXvP">Join My Discord</a>
+          </article>
+
+          <article>
+              <FaLinkedin className='contact__option-icon'/>
+              <h4>LinkedIn</h4>
+              <h5>https://www.linkedin.com/in/herman-rademeyer-a14aa119a</h5>
+              <a href="https://www.linkedin.com/in/herman-rademeyer-a14aa119a">Connect on LinkedIn</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" placeholder='Your Message' required rows="7"></textarea>
+          <button type='submit' className='btn btn-primary'>Submit Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,19 +1,58 @@
 import React from 'react'
 import './project.css'
+import Messenger from '../../assets/messenger-image.jpg';
+import Gym from '../../assets/gym-sets.jpg';
+import Recipe from '../../assets/recipe-image.jpg';
+
+const data = [
+  {
+    id: 1,
+    image: Gym,
+    title: 'Full Workout Regime',
+    github: 'https;//github/com',
+    demo: 'https://www.google.co.za/'
+  },
+
+  {
+    id: 2,
+    image:  Messenger,
+    title: 'Private Messaging App',
+    github: 'https;//github/com',
+    demo: 'https://www.google.co.za/'
+  },
+
+  {
+    id: 3,
+    image: Recipe,
+    title: 'Recipes A to Z',
+    github: 'https;//github/com',
+    demo: 'https://www.google.co.za/'
+  },
+]
 
 const projects = () => {
   return (
     <section id='projects'>
       <h5>My Recent Projects</h5>
       <h2>Portfolio</h2>
-
-      <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <h3>Travel Advisor App</h3>
-            <a href=""></a>
-          </div>
-        </article>
+      
+    <div className='container portfolio__container'>
+      {
+        data.map(({id,image,title,github,demo}) =>{
+          return (
+            <article key={id} className='portfolio__item'>
+              <div className='portfolio__item-image'>
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className='portfolio__item-cta'>
+                <a href={github} target="_blank" className='btn'>Github Repository</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Live Demo View</a>
+              </div>
+            </article>
+          )
+        })
+      }
       </div>
     </section>
   )
